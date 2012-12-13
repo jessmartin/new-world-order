0fb09cf0a37be2a3764a313e8e7c5539640d2e9e
Jess Martin <jessmartin@gmail.com>
2012-12-13 20:15:17 +0545

    Updating gems

--- template.rb
+++ template.rb
@@ -70,18 +70,18 @@ gem gem_for_database unless options[:skip_activerecord]
 
 append_file 'Gemfile', <<-CODE
 \n
-gem 'haml', '3.1.6'
+gem 'haml', '3.1.7'
 gem 'configatron', '2.9.1'
-gem 'airbrake', '~> 3.1.2'
-gem 'factory_girl_rails', '3.5.0'
-gem 'jquery-rails', '2.0.2'
+gem 'airbrake', '~> 3.1.6'
+gem 'factory_girl_rails', '4.1.0'
+gem 'jquery-rails', '2.1.4'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group "assets" do
   gem 'sass-rails',   '~> 3.2.5'
   gem 'coffee-rails', '~> 3.2.2'
-  gem 'uglifier', '>= 1.2.6'
+  gem 'uglifier', '>= 1.3.0'
 end
 
 group "development" do
@@ -89,12 +89,12 @@ group "development" do
 end
 
 group "development", "test" do
-  gem 'rspec-rails', '~> 2.11.0'
-  gem 'mocha', '~> 0.12.0'
-  gem 'guard-rspec',    '~> 1.1.0', :require => false
-  gem 'guard-cucumber', '~> 1.2.0', :require => false
+  gem 'rspec-rails', '~> 2.12.0'
+  gem 'mocha', '~> 0.13.1'
+  gem 'guard-rspec',    '~> 2.3.1', :require => false
+  gem 'guard-cucumber', '~> 1.2.2', :require => false
   gem 'growl',          '~> 1.0.3', :require => false
-  gem 'rb-fsevent',     '~> 0.9.1', :require => false
+  gem 'rb-fsevent',     '~> 0.9.2', :require => false
   gem 'cucumber-rails', '~> 1.3.0', :require => false
 end
 CODE
